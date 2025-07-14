8b1ef9e5bb35afd98a90868ee8c4b2d9cb34de6f
nikola14323 <nak.milow@web.de>
2025-07-14 18:32:44 +0200

    Update modelLoader.js

--- modelLoader.js
+++ modelLoader.js
@@ -1,4 +1,4 @@
-loadChurchModelimport * as THREE from 'three';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
