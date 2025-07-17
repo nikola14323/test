b75f31c3da227665e11fac930058ee13d2b7c0bd
nikola14323 <nak.milow@web.de>
2025-07-18 00:18:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import './autoMeshCollision.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { WorldBuilder } from './worldBuilder.js';
